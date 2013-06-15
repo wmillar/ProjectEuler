556c5f12400655c76f3914749440323ed8fda503
Wesley Millar <w3sleym@gmail.com>
2013-06-14 19:27:53 -0700

    P028: improved solution

--- 028.py
+++ 028.py
@@ -1,11 +1,25 @@
-spiralSize, spiralSum, currentNum = 1001, 1, 1
-for i in xrange(2,spiralSize,2):
-    currentNum += i
-    spiralSum += currentNum
-    currentNum += i
-    spiralSum += currentNum
-    currentNum += i
-    spiralSum += currentNum
-    currentNum += i
-    spiralSum += currentNum
-print spiralSum
+'''
+Starting with the number 1 and moving to the right in a clockwise direction
+a 5 by 5 spiral is formed as follows:
+
+    21 22 23 24 25
+    20  7  8  9 10
+    19  6  1  2 11
+    18  5  4  3 12
+    17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+formed in the same way?
+'''
+
+def sumSpiralDiagonals(size):
+    n = 1
+    spiralSum = 1
+    for add in xrange(2, size, 2):
+        spiralSum += n * 4 + add * 10
+        n += add * 4
+    return spiralSum
+
+print sumSpiralDiagonals(1001)
