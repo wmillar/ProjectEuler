8e60db28ce96e83dff6e1cbd27a28e453b2c8f7a
Wesley Millar <w3sleym@gmail.com>
2013-06-30 14:14:58 -0700

    P048: simplified

--- 048.py
+++ 048.py
@@ -1,9 +1,6 @@
-# The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
-# Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+'''
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+'''
 
-totalSum = 0L
-i = 1
-while i <= 1000:
-    totalSum += i**i
-    i += 1
-print str(totalSum)[len(str(totalSum))-10:]
+print str(sum(pow(n, n) for n in xrange(1, 1001)))[-10:]
