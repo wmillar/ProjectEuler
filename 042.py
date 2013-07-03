ddb19aabfa8724835924b487002d56260ff97e0e
Wesley Millar <w3sleym@gmail.com>
2013-07-03 12:47:32 -0700

    P042: improved solution

--- 042.py
+++ 042.py
@@ -1,70 +1,34 @@
-# -*- coding: cp1252 -*-
 '''
-The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1);
+The nth term of the sequence of triangle numbers is given by, t_n = 1/2n(n+1);
 so the first ten triangle numbers are:
 
 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
 
 By converting each letter in a word to a number corresponding to its
-alphabetical position and adding these values we form a word value. For example,
-the word value for SKY is 19 + 11 + 25 = 55 = t10. If the word value is a
-triangle number then we shall call the word a triangle word.
+alphabetical position and adding these values we form a word value.
+For example, the word value for SKY is 19 + 11 + 25 = 55 = t_10. If the word
+value is a triangle number then we shall call the word a triangle word.
 
 Using words.txt (right click and 'Save Link/Target As...'), a 16K text file
 containing nearly two-thousand common English words, how many are triangle
 words?
 '''
-def wordValue(word):
-    global alphabet
-    value = 0
-    for char in word:
-        value += alphabet.index(char)+1
-    return value
+from itertools import count
 
-def genTriNumbers(limit):
-    numList = []
-    i = 1
-    while True:
-        tempResult = int(.5*i*(i+1))
-        if tempResult < limit:
-            numList.append(tempResult)
-        else:
-            return numList
-        i += 1
 
-f = open("words.txt","r")
-data = f.readline()
-f.close()
+def solve_eq(n):
+    return -0.5 + pow(0.25 - (2  * n), 0.5)
 
-alphabet = ("A","B","C","D","E","F","G","H","I","J","K","L","M", \
-            "N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
 
-wordList = []
-index2 = -1
-while True:
-    index1 = index2 + 1
-    try:
-        index1 = data.index('"',index1)
-        index2 = data.index('"',index1+1)
-        word = data[index1+1:index2]
-        wordList.append(word)
-    except:
-        break
+def t(n):
+    return int(0.5 * n * (n + 1))
 
-wordValues = []
-for word in wordList:
-    wordValues.append(wordValue(word))
-maxWValue = 0
-for value in wordValues:
-    if value > maxWValue:
-        maxWValue = value
-triangleList = genTriNumbers(maxWValue)
 
-totalTriWords = 0
-for value in wordValues:
-    try:
-        triangleList.index(value)
-        totalTriWords += 1
-    except:
-        pass
-print totalTriWords
+def isTriangleWord(word):
+    wordValue = sum(valueDict[c] for c in word)
+    return t(int(solve_eq(-wordValue))) == wordValue
+
+
+valueDict = dict(zip(map(chr, xrange(65, 91)), count(1)))
+wordList = open('words.txt', 'r').read().replace('"', '').split(',')
+print map(isTriangleWord, wordList).count(True)
