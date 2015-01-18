3cf228da3d1a24921b5d88e2719b940eebed2cde
Wesley Millar <w3sleym@gmail.com>
2015-01-17 18:46:14 -0800

    P001: run in constant time

--- 001.py
+++ 001.py
@@ -5,7 +5,16 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
-def sumOfMultiples(n, limit=1000):
-    return sum(xrange(n, limit, n))
+def arithSum(n):
+    return n * (n + 1) / 2
 
-print sumOfMultiples(3) + sumOfMultiples(5) - sumOfMultiples(15)
+
+def arithSumMult(m, n):
+    return arithSum(n // m) * m
+
+
+def multSum(a, b, n):
+    return arithSumMult(a, n) + arithSumMult(b, n) - arithSumMult(a * b, n)
+
+
+print multSum(3, 5, 999)
